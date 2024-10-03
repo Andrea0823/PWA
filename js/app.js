8f2dbe3892ca72c7866c95bdb6085d1a49fef20e
Andrea0823 <97360188+Andrea0823@users.noreply.github.com>
2024-10-03 00:23:46 +0000

    basico

---
+++ js/app.js
@@ -0,0 +1,1 @@
+document.getElementById('content').innerHTML = '<p>Contenido cargado exitosamente.</p>';
---
+++ service-worker/sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'app-shell-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/styles.css',
+  '/js/app.js',
+  '/images/logo.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Archivos cacheados');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;  // Retorna desde el caché
+        }
+        return fetch(event.request);  // Realiza la petición a la red
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
